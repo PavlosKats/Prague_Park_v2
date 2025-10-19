bff2a3ba218b90537a2d044cdc36e72821a44a44
PavlosKats <paulkatselis@gmail.com>
2025-10-19 09:52:09 +0200

    Add project files.

---
+++ Prague_Park_v2/Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prague_Park_v2.Models
+{
+    public class Vehicle
+    {
+        public string? LicensePlate { get; set; }
+
+        public int Size { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        public int PricePerHour { get; set; }
+
+        public Vehicle(string? licensePlate)
+        {
+            LicensePlate = licensePlate;
+        }
+    }
+}
---
+++ Prague_Park_v2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prague_Park_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
